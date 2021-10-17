dbc3e973853b58d6cd1fb5627516a3e0349073f6
unknown <muchenski.dev@gmail.com>
2021-10-16 23:34:05 -0300

    Arrumando o uso de generics

--- 06-generics/funcoesGenericas.ts
+++ 06-generics/funcoesGenericas.ts
@@ -16,66 +16,148 @@ console.log(funcaoGenerica<boolean>(true)); // true
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Função genérica com vários tipos:
+abstract class Pessoa {
+    private _nome: string;
 
-interface IPessoa {
-    nome: string;
+    constructor(nome: string) {
+        this._nome = nome;
+    }
+
+    get nome(): string {
+        return this._nome;
+    }
 }
 
-interface IPessoaFisica extends IPessoa {
-    cpf: string;
+class PessoaFisica extends Pessoa {
+    private _cpf: string;
+
+    constructor(nome: string, cpf: string) {
+        super(nome);
+        this._cpf = cpf;
+    }
+
+    get cpf(): string {
+        return this._cpf;
+    }
 }
 
-interface ICarro {
-    placa: string;
-    valor: number;
+class PessoaJuridica extends Pessoa {
+    private _cnpj: string;
+
+    constructor(nome: string, cnpj: string) {
+        super(nome);
+        this._cnpj = cnpj;
+    }
+
+    get cnpj(): string {
+        return this._cnpj;
+    }
+}
+
+class Carro {
+    private _placa: string;
+    private _valor: number;
+
+    constructor(placa: string, valor: number) {
+        this._placa = placa;
+        this._valor = valor;
+    }
+
+    get placa(): string {
+        return this._placa;
+    }
+
+    get valor(): number {
+        return this._valor;
+    }
 }
 
 interface IMulta {
-    descricao: string;
-    numeroDePontosNaCarteira: number;
-    valorFinal?: number;
-    placaCarroMultado?: string;
-    identificador?: string;
-    calcular(carro: ICarro): void;
+    calcular(carro: Carro): void;
 }
 
-function gerarMulta<P extends IPessoaFisica, C extends ICarro, M extends IMulta>(pessoa: P, carro: C, multa: M): M {
-    multa.identificador = pessoa.cpf;
+class Multa implements IMulta {
+    private _descricao: string;
+    private _numeroDePontosNaCarteira: number;
+    private _valorFinal?: number | undefined;
+    private _placaCarroMultado?: string | undefined;
+    private _identificador?: string | undefined;
+
+    constructor(descricao: string, numeroDePontosNaCarteira: number) {
+        this._descricao = descricao;
+        this._numeroDePontosNaCarteira = numeroDePontosNaCarteira;
+    }
+
+    set valorFinal(valorFinal: number) {
+        this._valorFinal = valorFinal;
+    }
+
+    set identificador(identificador: string) {
+        this._identificador = identificador;
+    }
+
+    set placaCarroMultado(placaCarroMultado: string) {
+        this._placaCarroMultado = placaCarroMultado;
+    }
+
+    calcular(carro: Carro): void {
+        this._valorFinal = carro.valor * 0.005;
+    }
+}
+
+function gerarMulta<P extends Pessoa, C extends Carro>(pessoa: P, carro: C, multa: Multa): Multa {
+
+    if (pessoa instanceof PessoaFisica) {
+        multa.identificador = pessoa.cpf;
+
+    } else if (pessoa instanceof PessoaJuridica) {
+        multa.identificador = pessoa.cnpj;
+
+    } else {
+        multa.identificador = "Sem identificador";
+    }
+
     multa.placaCarroMultado = carro.placa;
     multa.calcular(carro);
     return multa;
 }
 
-const pessoa: IPessoa = { nome: "Henrique" };
-const pessoaFisica: IPessoaFisica = pessoa as IPessoaFisica;
-pessoaFisica.cpf = "10311678971";
+const pessoaFisica = new PessoaFisica("Henrique", "10311678971");
+const pessoaJuridica = new PessoaJuridica("Google", "66.018.008/0001-93");
 
-const carro: ICarro = { placa: "ABC-123", valor: 300000 };
+const carro1 = new Carro("ABC-123", 300000);
+const carro2 = new Carro("CBA-321", 900000);
 
-const multa: IMulta = {
-    descricao: "Passou no sinal vermelho",
-    numeroDePontosNaCarteira: 2,
-    calcular(carro: ICarro): void {
-        this.valorFinal = carro.valor * 0.005;
-    }
-}
+const multa = new Multa("Passou no sinal vermelho", 2);
 
 // [OUT]:
 /*
-{
-  "descricao": "Passou no sinal vermelho",
-  "numeroDePontosNaCarteira": 2,
-  "identificador": "10311678971",
-  "placaCarroMultado": "ABC-123",
-  "valorFinal": 1500
+Multa: {
+  "_descricao": "Passou no sinal vermelho",
+  "_numeroDePontosNaCarteira": 2,
+  "_identificador": "10311678971",
+  "_placaCarroMultado": "ABC-123",
+  "_valorFinal": 1500
 } 
 */
-console.log(gerarMulta(pessoaFisica, carro, multa));
+console.log(gerarMulta(pessoaFisica, carro1, multa));
+
+
+// [OUT]
+/*
+Multa: {
+  "_descricao": "Passou no sinal vermelho",
+  "_numeroDePontosNaCarteira": 2,
+  "_identificador": "66.018.008/0001-93",
+  "_placaCarroMultado": "CBA-321",
+  "_valorFinal": 4500
+} 
+*/
+console.log(gerarMulta(pessoaJuridica, carro2, multa));
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
-function fun<T, U, V>(args1: T, args2: U, args3: V): V {
+function fun<T, U, V, P>(args1: T, args2: U, args3: V, args4: P): V {
     return args3;
 }
-console.log(fun<string, number, boolean>('teste', 1, true)) // true
+console.log(fun<string, number, boolean, PessoaFisica>('teste', 1, true, new PessoaFisica("Teste", "10311678971"))) // true
